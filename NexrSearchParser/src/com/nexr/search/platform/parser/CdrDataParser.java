06c10ea62bb0ffc86873424b239c426d96a186cb
David Woo <david.woo@nexr.com>
2011-07-25 19:04:11 +0900

    CdrDataParser 짜잘한 버그 수정.

--- NexrSearchParser/src/com/nexr/search/platform/parser/CdrDataParser.java
+++ NexrSearchParser/src/com/nexr/search/platform/parser/CdrDataParser.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 public class CdrDataParser {
 
     private final String _ENCODING = "EUC-KR";
-    private final String _SEPARATOR = ",";
 
     private final String _columnFilePath;
     private final String _dataFilePath;
@@ -39,7 +38,7 @@ public class CdrDataParser {
 
     /**
      * Column 명이 정의 되어 있는 파일을 읽어, ArrayList<String> 타입 으로 저장 한다.
-     * @throws IOException
+     * @throws IOException  컬럼 파일 이 없거나, 읽을 수 없을 경우에 에러가 난다.
      */
     private void loadColumnData() throws IOException {
         File file = new File(_columnFilePath);
@@ -48,13 +47,13 @@ public class CdrDataParser {
 
         String row;
         while((row = reader.readLine()) != null){
-            _arrColumnData.add(row = row.trim());
+            _arrColumnData.add(row.trim());
         }
     }
 
     /**
      * CDR Data 를 파싱 하여, File System 에 작성 한다.
-     * @throws IOException
+     * @throws IOException  DataFile 을 읽을 수 없을 때, 에러가 발생 한다.
      */
     public void start() throws IOException {
         File file = new File(_dataFilePath);
@@ -65,6 +64,7 @@ public class CdrDataParser {
         while((row = reader.readLine()) != null){
             LogRecord logRecord = new LogRecord();
 
+            String _SEPARATOR = ",";
             String[] cols = row.split(_SEPARATOR);
 
             for(int i = 0 ; i < cols.length ; i++) {
