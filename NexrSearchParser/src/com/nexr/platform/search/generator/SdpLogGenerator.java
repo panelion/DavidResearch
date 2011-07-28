c02c54433f94de4232453e3eeb68de42e2bc95a1
David Woo <david.woo@nexr.com>
2011-07-29 01:54:43 +0900

    LogGenerator 프로젝트 생성.

--- NexrSearchParser/src/com/nexr/platform/search/generator/SdpLogGenerator.java
+++ NexrSearchParser/src/com/nexr/platform/search/generator/SdpLogGenerator.java
@@ -1,7 +1,21 @@
 package com.nexr.platform.search.generator;
 
+import com.nexr.platform.search.utils.io.AppendRootInputStream;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,34 +24,88 @@ import java.util.Map;
  * Time: 11:02 AM
  * SDP LOG DATA 를 Generator 한다.
  * TimeStamp 와 Transaction id 값을 Unique 하게 생성 한다.
+ *
+ * Original Xml Data : 798,912 rows.
  */
 public class SdpLogGenerator {
 
-    private final int _roofCount;
-    private final String _saveFilePath;
-    private final BufferedReader _xmlFileReader;
     private Map<String, String> _mapColumnData;
+    private DocumentBuilderFactory _documentBuilderFactory;
+    private Transformer _trans;
+    private BufferedWriter _logWriter;
+    private final String _ip;
+    private final String _currentTime;
+    private int _logCount;
+
+    private Date _date;
+    private Calendar _cal;
+    private SimpleDateFormat _simpleDateFormat;
 
     private final String ENCODING = "utf-8";
+    private final String SEPARATOR = ".";
 
-    public SdpLogGenerator(String saveFilePath,String xmlFilePath, String columnFilePath, int roofCount) throws IOException {
 
-        _saveFilePath = saveFilePath;
-        _roofCount =roofCount;
+    public int getLogCount() {
+        return _logCount;
+    }
 
-        File file = new File(xmlFilePath);
-
-        if(!file.exists()) {
-            throw new IOException("Can't Find SDP xml File.");
-        }
-
-        _xmlFileReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), ENCODING));
-
+    public SdpLogGenerator(String saveFilePath, String columnFilePath, String ip) throws IOException, XMLStreamException {
+        /**
+         *  Column Define Data 정보를 불러와 Map 형태로 저장 한다.
+         */
         try {
+            _mapColumnData = new HashMap<String, String>();
             this.loadColumnFile(columnFilePath);
         } catch(IOException e){
             throw new IOException("Can't Find SDP Column Define File.");
         }
+
+        _documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+
+        /**
+         * 저장 할 파일을 새로 생성 한다.
+         */
+        try {
+            TransformerFactory _transFactory = TransformerFactory.newInstance();
+            _trans = _transFactory.newTransformer();
+
+            File saveFile = new File(saveFilePath);
+            saveFile.createNewFile();
+
+            FileWriter fw = new FileWriter(saveFile);
+            _logWriter = new BufferedWriter(fw);
+
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * IP 를 변환 한다.
+         */
+        ip = ip.replaceAll("//.", "//-");
+        _ip = ip;
+
+        /**
+         * 현재 시간을 생성 한다.
+         */
+        _simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        _date = new Date();
+        _currentTime = _simpleDateFormat.format(_date);
+
+        /**
+         * 한달 전의 날짜를 구한다.
+         */
+        _simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        _cal = Calendar.getInstance();
+        _cal.setTime(_date);
+        _cal.add(Calendar.MONTH , - 1);
+    }
+
+    private String getAddSecondDate(){
+        _cal.add(Calendar.SECOND , 1);
+        _date = _cal.getTime();
+        return _simpleDateFormat.format(_date);
     }
 
     /**
@@ -61,33 +129,247 @@ public class SdpLogGenerator {
         }
     }
 
-    public void start() throws IOException {
+    /**
+     * Xml Log Data 를 생성 한다.
+     *
+     * @param xmlFilePath   Xml Data 가 저장 되어 있는 파일 경로
+     * @param roofCount     반복 해서 읽을 횟수.
+     *
+     * @throws IOException  Xml Data File 이 없을 경우 발생
+     */
+    public void start(String xmlFilePath, int roofCount) throws IOException {
         String row;
-        for(int i = 0; i < _roofCount; i++){
-            while((row = _xmlFileReader.readLine()) != null){
+
+        File file = new File(xmlFilePath);
+
+        if(!file.exists()) {
+            throw new IOException("Can't Find SDP xml File.");
+        }
+
+        for(int i = 0; i < roofCount; i++){
+
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), ENCODING);
+            BufferedReader xmlFileReader = new BufferedReader(inputStreamReader);
+
+            while((row = xmlFileReader.readLine()) != null){
+                // System.out.println(row);
                 if(!row.isEmpty()) {
-                    this.logGenerator(row);
+                    try {
+                        this.logGenerator(row);
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
                 }
+            }
+            System.out.println("start new reload");
+
+            xmlFileReader.close();
+            inputStreamReader.close();
+
+            row = null;
+        }
+    }
+
+    /**
+     * Document 를 생성 한다.
+     * @return  Document
+     */
+    private Document createDocument() {
+
+        Document document = null;
+        try {
+            DocumentBuilder documentBuilder;
+            documentBuilder = _documentBuilderFactory.newDocumentBuilder();
+            document  = documentBuilder.newDocument();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        return document;
+    }
+
+    /**
+     * String Xml Data 를 Element 형태로 변환 한다.
+     * @param xmlRow    String Xml Data.
+     * @return Element Xml Data Element
+     */
+    private Element getElementByStrXml(String xmlRow) {
+        try {
+            DocumentBuilder documentBuilder = _documentBuilderFactory.newDocumentBuilder();
+            Document readDocument = documentBuilder.parse(AppendRootInputStream.createInputStream(xmlRow));
+
+            return readDocument.getDocumentElement();
+
+        } catch (Exception e) {
+            System.err.println(xmlRow);
+        }
+
+        return null;
+    }
+
+    /**
+     * 기존의 Xml Log Data 를 이용 하여, 새로운 Log Data 를 생성 한다.
+     * @param xmlRow    String Xml Row Data;
+     */
+    private void logGenerator(String xmlRow) {
+
+        Element readElement = this.getElementByStrXml(xmlRow);
+
+        if(readElement == null) {
+            return;
+        }
+
+        if(readElement.hasChildNodes()) {
+
+            String firstNodeName = readElement.getNodeName(), secondNodeName,
+                    thirdNodeName, tempFirstNodeName = "", newNodeName;
+
+            String[] newNodeNames;
+
+            Document writeDoc = this.createDocument();
+
+            Element rootElement = null, secondElement;
+            NodeList nodeList = readElement.getChildNodes();
+
+            Node node, childNode;
+            ArrayList<Element> thirdElementList;
+
+            NodeList childNodeList;
+
+            for(int i = 0 ; i < nodeList.getLength(); i++) {
+
+                node = nodeList.item(i);
+
+                if(node.hasChildNodes()){
+
+                    secondNodeName = node.getNodeName();
+                    childNodeList = node.getChildNodes();
+
+                    thirdElementList = new ArrayList<Element>();
+
+                    for(int j = 0; j < childNodeList.getLength(); j++){
+
+                        childNode = childNodeList.item(j);
+
+                        if(childNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                            thirdNodeName = childNode.getNodeName();
+
+                            String textValue = childNode.getTextContent();
+                            // textValue = new String(textValue .getBytes("8859_1"), "UTF-8");
+
+                            String orgNodeName = firstNodeName + SEPARATOR + secondNodeName + SEPARATOR + thirdNodeName;
+
+                            if(_mapColumnData.containsKey(orgNodeName)) {
+                                newNodeName = _mapColumnData.get(orgNodeName);
+                                newNodeNames = newNodeName.split("\\" + SEPARATOR);
+                                if(newNodeNames.length > 0) {
+                                    newNodeName = newNodeNames[newNodeNames.length - 1];
+                                    if(!newNodeName.isEmpty()) {
+                                        thirdElementList.add(this.makeNewValue(writeDoc, newNodeName, textValue));
+                                        if(childNodeList.getLength() - 1 == j) {
+                                            secondNodeName = newNodeNames[1];
+                                            tempFirstNodeName = newNodeNames[0];
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    secondElement = writeDoc.createElement(secondNodeName);
+
+                    for(Element e : thirdElementList) {
+                        secondElement.appendChild(e);
+                    }
+
+                    if(i == 0 && rootElement == null && !tempFirstNodeName.isEmpty()) {
+                        rootElement = writeDoc.createElement(tempFirstNodeName);
+                    }
+
+                    rootElement.appendChild(secondElement);
+                }
+            }
+
+            try {
+                this.save(rootElement);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
 
-    public String logGenerator(String xmlRow) {
-        String rtnVal="";
+    /**
+     * 기존의 Element Value 를 버리고, 새로운 Element Value 를 customizing 한다.
+     * @param document  Document
+     * @param key       키 값
+     * @param value     Value
+     * @return          Element
+     */
+    private Element makeNewValue(Document document, String key, String value) {
 
-        return rtnVal;
+        Element element = document.createElement(key);
+        if(key.equals("PL")) {
+            element.appendChild(document.createCDATASection(value));
+        } else if(key.equals("TXID")) {
+            // ip + currentTimeStamp + "-" + "번호".
+            // 0308c137-543e-3ea8-b929-f699d4a00864
+            // UUID.randomUUID().toString(); or currentTimeStamp + 번호?
+            element.setTextContent(_ip + "-" + _currentTime + "-" + String.format("%09d", _logCount++));
+
+        } else if(key.equals("TS")){
+            // 2011-06-22 02:57:37.118
+            element.setTextContent(this.getAddSecondDate());
+
+        } else {
+            element.setTextContent(value);
+        }
+        return element;
     }
 
-    public void close(){
+
+    /**
+     * 변환 된 Xml Data 를 File 에 저장 한다.
+     * @param element   Xml Data Element.
+     */
+    private void save(Element element) {
         try {
-            _xmlFileReader.close();
+            StringWriter buffer = new StringWriter();
+
+            _trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            _trans.transform(new DOMSource(element), new StreamResult(buffer));
+
+            _logWriter.write(buffer.toString());
+            _logWriter.write(System.getProperty("line.separator"));
+
+            buffer = null;
+
+            _logWriter.flush();
+
+            if(_logCount % 10000 == 0)
+                System.out.println(_logCount);
+
+        } catch (TransformerException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void main(String[] args){
-        String saveFilePath, xmlFilePath, columnFilePath;
+    /**
+     * 모든 process 를 닫는다.
+     */
+    public void close(){
+        try {
+            _logWriter.flush();
+            _logWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws XMLStreamException, IOException {
+        String saveFilePath, xmlFilePath, columnFilePath, ip;
         int roofCount;
 
 
@@ -98,13 +380,22 @@ public class SdpLogGenerator {
             columnFilePath = args[2];
             roofCount = Integer.parseInt(args[3]);
 
+            ip = args[4];
+
         } else {
-            saveFilePath = "/home/david/Data/SearchPlatform/SDP/hdfs/";
-            xmlFilePath = "/home/david/Data/SearchPlatform/SDP/sdpXmlData.xml";
-            columnFilePath = "/home/david/Data/SearchPlatform/SDP/SdpColumnDefine.txt";
+            saveFilePath = "/Users/panelion/Data/SdpGenerateData.log";
+            xmlFilePath = "/Users/panelion/Data/sdpParseData.log";
+            columnFilePath = "/Users/panelion/Data/SdpColumnDefine.txt";
             roofCount = 10;
+            ip = "10.1.1.1";
         }
 
+        SdpLogGenerator sdpLogGenerator = new SdpLogGenerator(saveFilePath, columnFilePath, ip);
+        System.out.println("[Start Log Generator]");
+        sdpLogGenerator.start(xmlFilePath, roofCount);
+        sdpLogGenerator.close();
+        System.out.println("TOTAL ROW COUNT : " + sdpLogGenerator.getLogCount());
+        System.out.println("[End Log Generator]");
 
     }
 }
--- NexrSearchParser/src/com/nexr/platform/search/utils/io/AppendRootInputStream.java
+++ NexrSearchParser/src/com/nexr/platform/search/utils/io/AppendRootInputStream.java
@@ -11,7 +11,7 @@ import java.util.List;
  * User: david
  * Date: 7/5/11
  * Time: 5:02 PM
- * To change this template use File | Settings | File Templates.
+ * ROOT 가 없는 XML DATA 에 ROOT 를 씌워 주는 역할을 한다.
  */
 public class AppendRootInputStream extends SequenceInputStream {
 
@@ -20,7 +20,7 @@ public class AppendRootInputStream extends SequenceInputStream {
     }
 
     public static InputStream createInputStream(String path, String rootElement) throws FileNotFoundException {
-        return createInputStream(new File(path), rootElement);
+    return createInputStream(new File(path), rootElement);
     }
 
     public static InputStream createInputStream(File file, String rootElement) throws FileNotFoundException {
@@ -40,4 +40,15 @@ public class AppendRootInputStream extends SequenceInputStream {
 
         return new AppendRootInputStream(Collections.enumeration(inputStreamList));
     }
+
+    public static InputStream createInputStream(String xmlData) throws FileNotFoundException {
+        List<InputStream> inputStreamList = new ArrayList<InputStream>();
+
+        ByteArrayInputStream bodyInputStream = new ByteArrayInputStream(xmlData.getBytes());
+
+        inputStreamList.add(bodyInputStream);
+
+        return new AppendRootInputStream(Collections.enumeration(inputStreamList));
+    }
+
 }
