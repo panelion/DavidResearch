11593e4539bf51a39fc1e874cff616216ebf3f6e
David Woo <david.woo@nexr.com>
2011-08-01 13:49:46 +0900

    LogGenerator 주석 입력

--- NexrSearchParser/src/com/nexr/platform/search/generator/SdpXmlGenerator.java
+++ NexrSearchParser/src/com/nexr/platform/search/generator/SdpXmlGenerator.java
@@ -49,17 +49,19 @@ public class SdpXmlGenerator {
     private final String logRecordType;
 
     private final int idMaxCount;
+    private final int seqMaxCount;
 
     private String prefixTxLog;
 
     private final int startSeqNum = 1;
     private final int endSeqNum = 9999;
 
-    public SdpXmlGenerator(int idMaxCount, String moduleType, String serverName, String serverIP, String logRecordType) {
+    public SdpXmlGenerator(int idMaxCount, int seqMaxCount, String moduleType, String serverName, String serverIP, String logRecordType) {
 
         rnd = new Random();
 
         this.idMaxCount = idMaxCount;
+        this.seqMaxCount = seqMaxCount;
         this.serverName = serverName;
         this.serverIP = serverIP;
         this.moduleType = moduleType;
@@ -102,7 +104,7 @@ public class SdpXmlGenerator {
 
             this.makeDataHeaderData(entity.getDataHeaderEntity(), i, TXID, dateUtils.getAddSecond(1), objectName, methodName, strPayLoad.length());
 
-            if(i % this.idMaxCount == 0) txNum++;
+            if(i % this.seqMaxCount == 0) txNum++;
 
             /**
              * Body 부분 생성.
@@ -131,16 +133,26 @@ public class SdpXmlGenerator {
         entity.setScId(prefixScreenId + String.format("%04d", totalCount % idMaxCount));
     }
 
+    /**
+     * Header Data 를 생성 한다.
+     * @param entity        Data Header Entity.
+     * @param totalCount    만들어 질 로그 총 갯수.
+     * @param TXID          TransactionLog Nid
+     * @param strTimeStamp  TimeStamp.
+     * @param objectName    Object name
+     * @param methodName    Method Name
+     * @param payLoadSize   PayLoadSize.
+     */
     private void makeDataHeaderData(DataHeaderEntity entity, int totalCount, String TXID, String strTimeStamp, String objectName, String methodName, int payLoadSize) {
 
         entity.setTxId(TXID);
 
-        if(totalCount % idMaxCount == 1) {
+        if(totalCount % this.seqMaxCount == 1) {
             entity.setSeq(String.valueOf(startSeqNum));
-        } else if (totalCount % idMaxCount == 0) {
+        } else if (totalCount % this.seqMaxCount == 0) {
             entity.setSeq(String.valueOf(endSeqNum));
         } else {
-            entity.setSeq(String.valueOf(rnd.nextInt(endSeqNum - idMaxCount) + (idMaxCount - startSeqNum)));
+            entity.setSeq(String.valueOf(rnd.nextInt(endSeqNum - this.seqMaxCount) + (this.seqMaxCount - startSeqNum)));
         }
 
         entity.setTS(strTimeStamp);
@@ -168,23 +180,25 @@ public class SdpXmlGenerator {
 
     public static void main(String[] args) throws IOException, ParserConfigurationException {
 
-        int idMaxCount, xmlRowCount;
+        int userCount, seqMaxCount, xmlRowCount;
         String moduleType, serverName, serverIP, logRecordType, saveFilePath;
 
         if(args.length > 0) {
 
-            idMaxCount = Integer.parseInt(args[0]);
-            xmlRowCount = Integer.parseInt(args[1]);
+            userCount = Integer.parseInt(args[0]);
+            seqMaxCount = Integer.parseInt(args[1]);
+            xmlRowCount = Integer.parseInt(args[2]);
 
-            moduleType = args[2];
-            serverName = args[3];
-            serverIP = args[4];
-            logRecordType = args[5];
-            saveFilePath = args[6];
+            moduleType = args[3];
+            serverName = args[4];
+            serverIP = args[5];
+            logRecordType = args[6];
+            saveFilePath = args[7];
 
         } else {
-            idMaxCount = 8;
-            xmlRowCount = 11;
+            userCount = 100;
+            seqMaxCount = 8;
+            xmlRowCount = 1000000;
 
             // WAMUI/CSMUI, OCSG, SO, CSM, etc
             moduleType = "CSM";
@@ -194,11 +208,29 @@ public class SdpXmlGenerator {
 
             // IN_RES, OUT_RES, IN_REQ, OUT_REQ
             logRecordType = "IN_RES";
-            saveFilePath = "/home/david/Data/SearchPlatform/SDP/generateParseData.log";
+            saveFilePath = "/Users/Panelion/Data/generateParseData.log";
         }
 
         long startTime = System.currentTimeMillis();
-        SdpXmlGenerator sdpXmlGenerator = new SdpXmlGenerator(idMaxCount, moduleType, serverName, serverIP, logRecordType);
+
+
+        System.out.println("**********************************************************");
+
+        System.out.println("[USER COUNT : " + userCount + " ]");
+        System.out.println("[SEQ MAX COUNT : " + seqMaxCount + " ]");
+        System.out.println("[XML ROW COUNT : " + xmlRowCount + " ]");
+        System.out.println("[MODULE TYPE : " + moduleType + " ]");
+        System.out.println("[SERVER NAME : " + serverName + " ]");
+        System.out.println("[SERVER IP : " + serverIP + " ]");
+        System.out.println("[LOG RECORD TYPE : " + logRecordType + " ]");
+
+        System.out.println("[SAVE FILE PATH : " + saveFilePath + " ]");
+
+
+        System.out.println("**********************************************************");
+
+
+        SdpXmlGenerator sdpXmlGenerator = new SdpXmlGenerator(userCount, seqMaxCount, moduleType, serverName, serverIP, logRecordType);
         sdpXmlGenerator.generate(saveFilePath, xmlRowCount);
 
         long endTime = System.currentTimeMillis();
